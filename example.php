fa7831cd8400a67b6c4f43aa5245776590decb4d
EmanhMC <emanhmc@gmail.com>
2017-11-24 20:09:46 +0700

    Update example.php

--- example.php
+++ example.php
@@ -1,4 +1,11 @@
 <?php
-include ('getlink.php');
+
+include ('getlink.php'); //Includes the functions file.
+
+$link = "https://www.youtube.com/watch?v=YuIvDyPUC3g"; //Link of the YouTube video you want to GET
+
+getVideoYoutube($link); //Call the function
+
+//Yep, simple as fuck
 
 ?>
