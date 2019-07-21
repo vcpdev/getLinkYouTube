73790469d260c940c47ea118fd07945a8635513e
Valdesson Pereira Dos Santos <valdermito.to@gmail.com>
2019-07-21 20:02:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once('getlink.php'); //Includes the functions file.
 $link = "https://www.youtube.com/watch?v=bpt84ceWAY0"; //Link of the YouTube video you want to GET
-getVideoYoutube($link); //Call the function
+echo getVideoYoutube($link); //Call the function
 //Yep, simple as fuck
 ?>
